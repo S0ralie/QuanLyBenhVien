3314ed5db1daceca5e09363335d42074f08d5902
S0ralie <hungbq2206@gmail.com>
2026-04-23 12:18:23 +0700

    db

--- DataMau.sql
+++ DataMau.sql
@@ -1,117 +1,82 @@
-USE QuanLyBenhVien;
+USE [QuanLyBenhVien];
 GO
 
-INSERT INTO Khoa (MaKhoa, TenKhoa) VALUES 
-('K01', N'Khoa Nội Tổng Quát'),
-('K02', N'Khoa Ngoại Chấn Thương'),
-('K03', N'Khoa Nhi'),
-('K04', N'Khoa Sản Phụ Khoa'),
-('K05', N'Khoa Tai Mũi Họng'),
-('K06', N'Khoa Răng Hàm Mặt'),
-('K07', N'Khoa Tim Mạch');
+DELETE FROM HoaDonThanhToan;
+DELETE FROM ChiTietDonThuoc;
+DELETE FROM DonThuoc;
+DELETE FROM PhieuKetQuaKhamBenh;
+DELETE FROM PhieuDangKyKham;
+DELETE FROM TaiKhoan; -- Xóa sạch tài khoản trước
+DELETE FROM BacSi;
+DELETE FROM NhanVienYTe;
+DELETE FROM HoSoBenhAn;
+DELETE FROM BenhNhan;
+DELETE FROM Thuoc;
+DELETE FROM Quyen;
+DELETE FROM Khoa;
+GO
 
-INSERT INTO Quyen (MaQuyen, TenQuyen) VALUES 
-('ADMIN', N'Quản trị hệ thống'),
-('BACSI', N'Bác sĩ chuyên khoa'),
-('BENHNHAN', N'Bệnh nhân');
+-- Khoa & Thuốc
+INSERT INTO Khoa (MaKhoa, TenKhoa, ViTri, SDT) VALUES 
+('K01', N'Khoa Nội', N'Tầng 2 - Tòa A', '0243123456'),
+('K02', N'Khoa Ngoại', N'Tầng 3 - Tòa B', '0243654321');
 
-INSERT INTO Thuoc (MaThuoc, TenThuoc, DonGia) VALUES 
-('T01', N'Paracetamol 500mg', 1500),
-('T02', N'Amoxicillin 500mg', 3000),
-('T03', N'Panadol Extra', 2500),
-('T04', N'Vitamin C 1000mg', 5000),
-('T05', N'Augmentin 1g', 15000);
+INSERT INTO Thuoc (MaThuoc, TenThuoc, DonVi, DonGia) VALUES 
+('T01', N'Paracetamol 500mg', N'Viên', 2000),
+('T02', N'Amoxicillin', N'Viên', 5000),
+('T03', N'Vitamin C', N'Viên', 1000);
 
-INSERT INTO BenhNhan (MaBN, HoTen, CCCD) VALUES 
-('BN001', N'Nguyễn Văn An', '0123456789'),
-('BN002', N'Trần Thị Bình', '0123456789'),
-('BN003', N'Lê Hoàng Nam', '0123456789'),
-('BNOO4', N'Nguyễn Trần Long', '1238023847');
+-- Quyền & Bệnh Nhân
+INSERT INTO Quyen (MaQuyen, TenQuyen, MoTa) VALUES 
+('ADMIN', N'Quản trị viên', N'Toàn quyền hệ thống'),
+('BACSI', N'Bác sĩ', N'Khám bệnh và kê đơn'),
+('LETAN', N'Lễ tân', N'Tiếp đón bệnh nhân');
 
-INSERT INTO NhanVienYTe (MaNV, HoTen, MaKhoa) VALUES ('NV01', N'BS. Phạm Thành Long', 'K07');
-INSERT INTO BacSi (MaNV, ChuyenMon, BangCap) VALUES ('NV01', N'Nội Tim Mạch', N'Tiến sĩ - Bác sĩ');
+INSERT INTO BenhNhan (MaBN, HoTen, CCCD, NgaySinh, GioiTinh, DiaChi, SDT, BHYT) VALUES 
+('BN01', N'Nguyễn Văn A', '001200123456', '1995-05-20', N'Nam', N'Hà Nội', '0912345678', 'GD40101'),
+('BN02', N'Trần Thị B', '001200654321', '1998-10-15', N'Nữ', N'Hà Nam', '0988888888', 'GD40102');
 
-INSERT INTO NhanVienYTe (MaNV, HoTen, MaKhoa) VALUES ('NV02', N'BS. Lê Minh Hương', 'K03');
-INSERT INTO BacSi (MaNV, ChuyenMon, BangCap) VALUES ('NV02', N'Nhi Khoa', N'Thạc sĩ - Bác sĩ CK I');
+-- Nhân Viên & Bác Sĩ
+INSERT INTO NhanVienYTe (MaNV, HoTen, CCCD, NgaySinh, GioiTinh, SDT, ChucVu, MaKhoa) VALUES 
+('NV01', N'Bác sĩ Trương Gia Bình', '001090123456', '1975-01-01', N'Nam', '0901112222', N'Trưởng khoa', 'K01'),
+('NV02', N'Bác sĩ Lê Thanh Hải', '001090654321', '1980-05-05', N'Nam', '0903334444', N'Bác sĩ điều trị', 'K02');
 
-INSERT INTO TaiKhoan (TenDangNhap, MatKhau, MaQuyen, MaNV, MaBN) VALUES 
-('admin', '123456', 'ADMIN', NULL, NULL),
-('bacsi1', '123456', 'BACSI', 'NV01', NULL),
-('benhnhan1', '123456', 'BENHNHAN', NULL, 'BN001');
+INSERT INTO BacSi (MaNV, ChuyenMon, BangCap, SoChungChiHanhNghe) VALUES 
+('NV01', N'Nội tổng quát', N'Tiến sĩ', 'CCHN001'),
+('NV02', N'Ngoại khoa', N'Thạc sĩ', 'CCHN002');
 
-INSERT INTO HoSoBenhAn (MaHS, MaBN) VALUES 
-('HS001', 'BN001'),
-('HS002', 'BN002');
+-- Tài Khoản & Hồ Sơ Bệnh Án
+INSERT INTO TaiKhoan (TenDangNhap, MatKhau, MaQuyen, MaNV) VALUES 
+('admin', '123456', 'ADMIN', NULL),
+('bacsi1', '123456', 'BACSI', 'NV01'),
+('bacsi2', '123456', 'BACSI', 'NV02');
 
+INSERT INTO HoSoBenhAn (MaHS, NgayTao, NhomMau, TienSuBenh, MaBN) VALUES 
+('HS01', GETDATE(), 'O', N'Dị ứng hải sản', 'BN01'),
+('HS02', GETDATE(), 'A', N'Không có', 'BN02');
+
+-- E. Phiếu Đăng Ký & Phiếu Kết Quả (Nghiệp vụ khám)
 INSERT INTO PhieuDangKyKham (MaPhieuDK, NgayKham, GioKham, TrangThai, MaBN, MaNV) VALUES 
-('DK001', '2026-04-21', '08:00', N'Chờ Khám', 'BN001', 'NV01'),
-('DK002', '2026-04-21', '08:30', N'Đã Đến', 'BN002', 'NV01'),
-('DK003', '2026-04-21', '09:00', N'Khám Xong', 'BN003', 'NV02');
+('PDK01', GETDATE(), '08:30:00', N'Đã Khám', 'BN01', 'NV01'),
+('PDK02', GETDATE(), '09:15:00', N'Chờ Khám', 'BN02', 'NV01');
 
 INSERT INTO PhieuKetQuaKhamBenh (MaPhieuKQ, NgayKham, TrieuChung, ChanDoan, MaHS, MaNV) VALUES 
-('KQ001', '2026-04-20', N'Đau ngực, khó thở nhẹ', N'Rối loạn nhịp tim nhẹ', 'HS001', 'NV01');
+('PKQ01', GETDATE(), N'Sốt, ho khan', N'Viêm họng cấp', 'HS01', 'NV01');
 
-INSERT INTO DonThuoc (MaDonThuoc, MaPhieuKQ) VALUES ('DT001', 'KQ001');
-INSERT INTO ChiTietDonThuoc (MaDonThuoc, MaThuoc, SoLuong) VALUES 
-('DT001', 'T01', 10),
-('DT001', 'T04', 5);
+-- Đơn Thuốc & Chi Tiết
+INSERT INTO DonThuoc (MaDonThuoc, NgayKeDon, MaPhieuKQ) VALUES 
+('DT01', GETDATE(), 'PKQ01');
+
+INSERT INTO ChiTietDonThuoc (MaDonThuoc, MaThuoc, SoLuong, LieuLuong) VALUES 
+('DT01', 'T01', 10, N'Ngày uống 2 lần, mỗi lần 1 viên'),
+('DT01', 'T02', 14, N'Ngày uống 2 lần sau ăn');
+
+-- Hóa Đơn
+INSERT INTO HoaDonThanhToan (MaHD, TongTien, MucGiamBHYT, TrangThai, MaBN) VALUES 
+('HD01', 150000, 0.8, N'Đã Thanh Toán', 'BN01');
 GO
 
-USE QuanLyBenhVien;
-GO
-
-INSERT INTO Khoa (MaKhoa, TenKhoa) VALUES 
-('K01', N'Khoa Nội Tổng Quát'),
-('K02', N'Khoa Ngoại Chấn Thương'),
-('K03', N'Khoa Nhi'),
-('K04', N'Khoa Sản Phụ Khoa'),
-('K05', N'Khoa Tai Mũi Họng'),
-('K06', N'Khoa Răng Hàm Mặt'),
-('K07', N'Khoa Tim Mạch');
-
-INSERT INTO Quyen (MaQuyen, TenQuyen) VALUES 
-('ADMIN', N'Quản trị hệ thống'),
-('BACSI', N'Bác sĩ chuyên khoa'),
-
-INSERT INTO Thuoc (MaThuoc, TenThuoc, DonGia) VALUES 
-('T01', N'Paracetamol 500mg', 1500),
-('T02', N'Amoxicillin 500mg', 3000),
-('T03', N'Panadol Extra', 2500),
-('T04', N'Vitamin C 1000mg', 5000),
-('T05', N'Augmentin 1g', 15000);
-
-INSERT INTO BenhNhan (MaBN, HoTen, CCCD) VALUES 
-('BN001', N'Nguyễn Văn An', '0123456789'),
-('BN002', N'Trần Thị Bình', '0123456789'),
-('BN003', N'Lê Hoàng Nam', '0123456789'),
-('BNOO4', N'Nguyễn Trần Long', '1238023847');
-
-INSERT INTO NhanVienYTe (MaNV, HoTen, MaKhoa) VALUES ('NV01', N'BS. Phạm Thành Long', 'K07');
-INSERT INTO BacSi (MaNV, ChuyenMon, BangCap) VALUES ('NV01', N'Nội Tim Mạch', N'Tiến sĩ - Bác sĩ');
-
-INSERT INTO NhanVienYTe (MaNV, HoTen, MaKhoa) VALUES ('NV02', N'BS. Lê Minh Hương', 'K03');
-INSERT INTO BacSi (MaNV, ChuyenMon, BangCap) VALUES ('NV02', N'Nhi Khoa', N'Thạc sĩ - Bác sĩ CK I');
-
-INSERT INTO TaiKhoan (TenDangNhap, MatKhau, MaQuyen, MaNV, MaBN) VALUES 
-('admin', '123456', 'ADMIN', NULL, NULL),
-('bacsi1', '123456', 'BACSI', 'NV01', NULL),
-
-INSERT INTO HoSoBenhAn (MaHS, MaBN) VALUES 
-('HS001', 'BN001'),
-('HS002', 'BN002');
-
-INSERT INTO PhieuDangKyKham (MaPhieuDK, NgayKham, GioKham, TrangThai, MaBN, MaNV) VALUES 
-('DK001', '2026-04-21', '08:00', N'Chờ Khám', 'BN001', 'NV01'),
-('DK002', '2026-04-21', '08:30', N'Đã Đến', 'BN002', 'NV01'),
-('DK003', '2026-04-21', '09:00', N'Khám Xong', 'BN003', 'NV02');
-
-INSERT INTO PhieuKetQuaKhamBenh (MaPhieuKQ, NgayKham, TrieuChung, ChanDoan, MaHS, MaNV) VALUES 
-('KQ001', '2026-04-20', N'Đau ngực, khó thở nhẹ', N'Rối loạn nhịp tim nhẹ', 'HS001', 'NV01');
-
-INSERT INTO DonThuoc (MaDonThuoc, MaPhieuKQ) VALUES ('DT001', 'KQ001');
-INSERT INTO ChiTietDonThuoc (MaDonThuoc, MaThuoc, SoLuong) VALUES 
-('DT001', 'T01', 10),
-('DT001', 'T04', 5);
-GO
-
-COMMIT;
+-- KIỂM TRA
+SELECT * FROM TaiKhoan;
+SELECT * FROM PhieuKetQuaKhamBenh;
+SELECT * FROM ChiTietDonThuoc;
